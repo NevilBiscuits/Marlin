3e240e0cba1cd14a688551fe95b396cd52f74bc2
NevilBiscuits <62939552+NevilBiscuits@users.noreply.github.com>
2020-10-29 16:53:28 +1100

    Probe Speed Update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -960,7 +960,7 @@
 #define MIN_PROBE_EDGE 25
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 10000
+#define XY_PROBE_SPEED 14000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
